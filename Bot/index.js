1b8754f74ca465bc0b7666b91f54e23c02879dd9
Steven <steven.popick@gmail.com>
2019-03-17 01:26:14 -0500

    Making improvments to the feedback of the bot.Removed Join voice channel

--- Bot/index.js
+++ Bot/index.js
@@ -1,51 +1,28 @@
 /* eslint-disable no-console */
 const Discord = require('discord.js')
-const { prefix, token } = require('./config.json');
+const { prefix, token } = require('./config.json'); 
 const client = new Discord.Client()
 
 client.on('ready', () => {
-    console.log("Connected as " + client.user.tag)
+
+    channels = client.channels.find(channels => channels.name === 'general'); // find the name of the 'general' text channel
+    console.log(channels);
+    console.log("I'm Ready");
+
+    channels.send("I'm ready! (testing)"); // output a message saying the bot is ready for action!
 })
 
-// client.on('message', message => {
-//     let username = message.member.user.username;
-//     if (message != '' && username === 'Jiroscopes'){
-//         message.channel.send('Yes king');
-//     } else if (message != '' && username !='Jiroscopes' && !message.author.bot){
-//         // message.channel.send();
-//         return;
-//     }
-// });
-
-// client.on('voiceStateUpdate', (oldMember, newMember) => {
-//     let newUserChannel = newMember.voiceChannel
-//     let oldUserChannel = oldMember.voiceChannel
-//     let userVoiceChannel = newMember.voiceChannelID;
-  
-//     if(oldUserChannel === undefined && newUserChannel !== undefined) {
-//        if (!userVoiceChannel) return console.error("The channel does not exist");
-//             client.channels.get(userVoiceChannel).join(connection => {
-//                 console.log("Connected");
-//             }).catch(e => {
-//                 console.error(e);
-//             });
-//         } else if(newUserChannel === undefined){
-//             console.log('User left');
-//                 client.voiceChannel.leave();
-//             }
-//     });
-
-
+// This is a leave server command
 client.on('message', message => {
     if (message.content === '!leave'){
        let guild = message.guild;
-    //    console.log(guild);
         message.channel.send("By-O");
-        guild.leave();
+        guild.leave(); // leave server
     } else if (message.content === '!showMeGod'){
-        let avatar = client.user.avatarURL;
+        let avatar = client.user.avatarURL; // get avatar URL
         message.channel.send(avatar);
     }
 });
 
-  client.login(token);
+
+  client.login(token); // Login to the server
